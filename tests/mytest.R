4a96b67c51ed8e567d2c9b037a32d52b0f7caeef
rmpinto <52088176+rmpinto@users.noreply.github.com>
2019-10-23 02:08:18 +0100

    Testing

--- tests/mytest.R
+++ tests/mytest.R
@@ -1,8 +1,4 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest")
 
-app$setInputs(speciesInput = "Merluccius merlucciu")
-app$setInputs(speciesInput = "Merluccius merluccius")
-app$snapshot()
-app$setInputs(addSpecies = "click", wait_=TRUE, timeout_=600000)
 app$snapshot()
